bfabfef4442e47b3e518405c92d6ba22ad75c7aa
Luca Bercioux <Lucabex@gmail.com>
2026-07-23 23:23:30 +0100

    Add router

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import {Routes,Route} from 'react-router-dom'
 import './App.css'
 
 
@@ -10,6 +11,10 @@ function App() {
 
   return (
     <>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<About />}/>
+    </Routes>
       
 
     </>
