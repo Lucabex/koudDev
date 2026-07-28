40cb4a1f23005160c15c81b1ebf5ce4c257afc74
Luca Bercioux <Lucabex@gmail.com>
2026-07-28 20:09:42 +0100

    Add email form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -363,7 +363,15 @@ function Home(){
                                 >
                                     Luton , Bedfordshire ,England ,Uk
                                 </a> </span>
-                    <span><Mail size={14}/> Lucabex@gmail.com</span>
+                  <span className="flex items-center gap-2">
+  <Mail size={14} />
+  
+   
+  <a  href="mailto:lucabex@gmail.com?subject=New%20Project%20Enquiry&body=KOUDELKADEV%0A%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%0ANew%20Project%20Enquiry%0A%0APlease%20complete%20every%20field%20below%20and%20send%20this%20email%20back%20to%20us.%20The%20more%20detail%20you%20give%2C%20the%20more%20accurate%20your%20quote%20will%20be.%0A%0ACONTACT%0AName%3A%20%0ALast%20name%3A%20%0ABusiness%20name%3A%20%0AEmail%3A%20%0APhone%3A%20%0A%0APROJECT%0AProject%20type%3A%20%0ABudget%3A%20%0ATimeline%3A%20%0A%0ADETAILS%0AProject%20details%3A%20%0A%0A%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%0AThank%20you%2C%0Akoudelkadev"
+    className="hover:text-sky-400 hover:underline transition-colors">
+    lucabex@gmail.com
+  </a>
+</span>
                 </div>
             </div>
         </section>
